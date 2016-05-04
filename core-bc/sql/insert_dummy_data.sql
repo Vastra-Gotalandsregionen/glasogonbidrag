9ab2b935c8da1d04882504f0a4ba75aa250860ce
Martin Lind <martin@spegelreflex.se>
2016-05-04 15:09:07 +0200

    SQL statement to insert dummy suppliers.

---
+++ core-bc/sql/insert_dummy_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO vgr_glasogonbidrag_supplier
+VALUES ('Synsam Alfa'),
+       ('Synsam Beta'),
+       ('Synsam Gamma'),
+       ('Synsam Delta'),
+       ('Specsavers Alfa'),
+       ('Specsavers Beta'),
+       ('Specsavers Gamma'),
+       ('Specsavers Delta'),
+       ('Wasaoptik'),
+       ('Direktoptik'),
+       ('Hendéns');
+
