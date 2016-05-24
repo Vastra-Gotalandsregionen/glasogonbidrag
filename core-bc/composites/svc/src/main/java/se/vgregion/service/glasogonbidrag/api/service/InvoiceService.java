a95e881bcc7519465642c76ddf00a384d97ac901
Martin Lind <martin@spegelreflex.se>
2016-05-24 11:11:32 +0200

    WIP: Added service method.

--- core-bc/composites/svc/src/main/java/se/vgregion/service/glasogonbidrag/api/service/InvoiceService.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/service/glasogonbidrag/api/service/InvoiceService.java
@@ -2,6 +2,7 @@ package se.vgregion.service.glasogonbidrag.api.service;
 
 import se.vgregion.portal.glasogonbidrag.domain.jpa.Grant;
 import se.vgregion.portal.glasogonbidrag.domain.jpa.Invoice;
+import se.vgregion.service.glasogonbidrag.exception.GrantAlreadyExistException;
 
 /**
  * @author Martin Lind - Monator Technologies AB
@@ -10,6 +11,10 @@ public interface InvoiceService {
     void create(Invoice invoice);
 
     void update(Invoice invoice);
+
+    void updateWithGrant(long userId, long groupId, long companyId,
+                         Invoice invoice, Grant grant)
+            throws GrantAlreadyExistException;
 
     void delete(Long id);
 }
--- core-bc/composites/svc/src/main/java/se/vgregion/service/glasogonbidrag/internal/service/InvoiceServiceImpl.java
+++ core-bc/composites/svc/src/main/java/se/vgregion/service/glasogonbidrag/internal/service/InvoiceServiceImpl.java
@@ -4,11 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import se.vgregion.portal.glasogonbidrag.domain.jpa.Grant;
 import se.vgregion.portal.glasogonbidrag.domain.jpa.Invoice;
 import se.vgregion.service.glasogonbidrag.api.service.InvoiceService;
+import se.vgregion.service.glasogonbidrag.exception.GrantAlreadyExistException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Martin Lind - Monator Technologies AB
@@ -27,6 +32,10 @@ public class InvoiceServiceImpl implements InvoiceService {
     public void create(Invoice invoice) {
         LOGGER.info("Persisting invoice: {}", invoice);
 
+        // Update creation date and modification date
+        Calendar cal = Calendar.getInstance();
+        Date date = cal.getTime();
+
         em.persist(invoice);
     }
 
@@ -35,7 +44,47 @@ public class InvoiceServiceImpl implements InvoiceService {
     public void update(Invoice invoice) {
         LOGGER.info("Updating invoice: {}", invoice);
 
+        // Update modification date of this invoice
+        Calendar cal = Calendar.getInstance();
+        Date date = cal.getTime();
+
+        invoice.setModifiedDate(date);
+
         em.merge(invoice);
+    }
+
+    @Override
+    @Transactional
+    public void updateWithGrant(long userId, long groupId, long companyId,
+                                Invoice invoice, Grant grant)
+            throws GrantAlreadyExistException {
+        LOGGER.info("Add grant {} to invoice {}", invoice, grant);
+
+        List<Grant> grants = invoice.getGrants();
+        if (grants == null) {
+            throw new RuntimeException("List of grants is null");
+        }
+
+        if (grants.contains(grant)) {
+            throw new GrantAlreadyExistException("Grant is already set.");
+        }
+
+        // Set user, group and company id of new grant.
+        grant.setUserId(userId);
+        grant.setGroupId(groupId);
+        grant.setCompanyId(companyId);
+
+        // Update creation date and modification date of new grant.
+        Calendar cal = Calendar.getInstance();
+        Date date = cal.getTime();
+
+        grant.setCreateDate(date);
+        grant.setModifiedDate(date);
+
+        // Add grant to the invoice object and update it.
+        invoice.addGrant(grant);
+
+        this.update(invoice);
     }
 
     @Override
