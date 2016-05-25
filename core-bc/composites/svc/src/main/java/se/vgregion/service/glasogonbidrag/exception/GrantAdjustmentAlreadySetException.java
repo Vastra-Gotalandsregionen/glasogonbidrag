fbe5f601e670374cde19e5f1c83d4838cc835a02
Martin Lind <martin@spegelreflex.se>
2016-05-25 15:07:06 +0200

    Added method to invoice service to add grant adjustment.

---
+++ core-bc/composites/svc/src/main/java/se/vgregion/service/glasogonbidrag/exception/GrantAdjustmentAlreadySetException.java
@@ -0,0 +1,25 @@
+package se.vgregion.service.glasogonbidrag.exception;
+
+/**
+ * @author Martin Lind - Monator Technologies AB
+ */
+public class GrantAdjustmentAlreadySetException extends Exception {
+    public GrantAdjustmentAlreadySetException() {
+    }
+
+    public GrantAdjustmentAlreadySetException(String message) {
+        super(message);
+    }
+
+    public GrantAdjustmentAlreadySetException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GrantAdjustmentAlreadySetException(Throwable cause) {
+        super(cause);
+    }
+
+    public GrantAdjustmentAlreadySetException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
