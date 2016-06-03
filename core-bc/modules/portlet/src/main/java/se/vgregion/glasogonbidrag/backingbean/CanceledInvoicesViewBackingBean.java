2bed4646a78b03c3ecf8b81b9974ea266d76b7c4
Martin Lind <martin@spegelreflex.se>
2016-06-03 18:03:01 +0200

    Added naviation object.

--- core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/backingbean/CanceledInvoicesViewBackingBean.java
+++ core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/backingbean/CanceledInvoicesViewBackingBean.java
@@ -1,7 +1,13 @@
 package se.vgregion.glasogonbidrag.backingbean;
 
 
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.PortletURLFactoryUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -11,12 +17,17 @@ import se.vgregion.portal.glasogonbidrag.domain.jpa.InvoiceStatus;
 import se.vgregion.service.glasogonbidrag.api.data.InvoiceRepository;
 
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Locale;
 
 @Component(value = "canceledInvoicesViewBackingBean")
 @Scope(value = "request")
 public class CanceledInvoicesViewBackingBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CanceledInvoicesViewBackingBean.class);
 
     @Autowired
     private InvoiceRepository invoiceRepository;
@@ -39,6 +50,8 @@ public class CanceledInvoicesViewBackingBean {
     protected void init() {
 
         ThemeDisplay themeDisplay = facesUtil.getThemeDisplay();
+//        HttpServletRequest request = themeDisplay.getRequest();
+        long groupId = themeDisplay.getScopeGroupId();
         locale = themeDisplay.getLocale();
         // Temporary - make sure we always get Swedish locale
         locale = Locale.forLanguageTag("sv-SE");
