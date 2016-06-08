82aa0e97e0431e28517e8e57d722ce154c575ce4
Martin Lind <martin@spegelreflex.se>
2016-06-08 12:43:36 +0200

    Drop scripts.

--- core-bc/sql/drop_tables.sql
+++ core-bc/sql/drop_tables.sql
@@ -1,7 +1,15 @@
 
 -- Drop tables
 DROP TABLE vgr_glasogonbidrag_grant;
-DROP TABLE vgr_glasogonbidrag_grant_adjustment;
 DROP TABLE vgr_glasogonbidrag_invoice;
 DROP TABLE vgr_glasogonbidrag_beneficiary;
+
+DROP TABLE vgr_glasogonbidrag_personal_identification;
+DROP TABLE vgr_glasogonbidrag_lma_identification;
+DROP TABLE vgr_glasogonbidrag_identification;
+
+DROP TABLE vgr_glasogonbidrag_grant_adjustment;
+
+DROP TABLE vgr_glasogonbidrag_prescription;
+
 DROP TABLE vgr_glasogonbidrag_supplier;
