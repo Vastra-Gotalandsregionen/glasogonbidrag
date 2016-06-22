a6eca47d39b04bc19509599fdc15cd8da856b866
Martin Lind <martin@spegelreflex.se>
2016-06-22 15:22:08 +0200

    Identification util uses regex, commented code and various code formating.

--- core-bc/modules/application/src/main/java/se/vgregion/glasogonbidrag/util/DocumentUtil.java
+++ core-bc/modules/application/src/main/java/se/vgregion/glasogonbidrag/util/DocumentUtil.java
@@ -7,25 +7,95 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * Utility class for helping of building Import Document and Import Grant.
+ * Also helpers for related functionality.
+ * 
  * @author Martin Lind - Monator Technologies AB
  */
 public final class DocumentUtil {
-    private DocumentUtil() {
 
-    }
+    /**
+     * Prevent initialization of this class.
+     */
+    private DocumentUtil() {}
 
+    /**
+     * Build a local grant object form an row, the fields in the array
+     * have the following mapping:
+     *
+     * row[0] contains the name of the person (document)
+     *
+     * The id is present on a previous row.
+     *
+     * @param id the id of the document.
+     * @param row data for an import document.
+     * @return an import grant that should be converted to
+     *         domain objects and inserted in the database.
+     */
     public static ImportDocument document(String id, String[] row) {
         return document(id, row[0]);
     }
 
-    private static ImportDocument document(String id, String name) {
-        return new ImportDocument(id, name);
-    }
-
+    /**
+     * Build a local grant object form an array of strings, the fields in
+     * the array have the following mapping:
+     *
+     * row[1] contains date to set as prescription date.
+     * row[2] contains date to set as delivery date.
+     * row[3] contains amount including vat to set as amount for date.
+     * row[4] contains the invoice number, this may be blank.
+     * row[5] contains the verification number.
+     *
+     * @param row data for an import grant.
+     * @return an import grant that should be converted
+     */
     public static ImportGrant grant(String[] row) {
         return grant(row[1], row[2], row[3], row[4], row[5]);
     }
 
+    /**
+     * Build a local grant object form a strings.
+     *
+     * @param prescriptionDate date to set as prescription date.
+     * @param deliveryDate date to set as delivery date.
+     * @param amount amount including vat to set as amount for date
+     * @param invoiceNumber the invoice number, this may be blank
+     * @param verificationNumber the verification number
+     * @return an import grant that should be converted to
+     *         domain objects and inserted in the database.
+     */
+    private static ImportGrant grant(String prescriptionDate,
+                                     String deliveryDate,
+                                     String amount,
+                                     String invoiceNumber,
+                                     String verificationNumber) {
+        return new ImportGrant(
+                prescriptionDate, deliveryDate,
+                amount, invoiceNumber, verificationNumber);
+    }
+
+    /**
+     * Build a local document object from strings.
+     *
+     * @param id the id of this document
+     * @param name the name of this document
+     * @return an import document that should be converted to
+     *         domain objects and inserted in the database.
+     */
+    private static ImportDocument document(String id, String name) {
+        return new ImportDocument(id, name);
+    }
+
+    /**
+     * The comment (column 7) in an row may contain a text with the
+     * verification of the replacement invoice, this method will extract
+     * this verification number.
+     *
+     * A verification number should contain 8 numbers and no characters.
+     *
+     * @param comment the comment that contains the verification number.
+     * @return verification number as a string.
+     */
     public static String extractVerification(String comment) {
         Pattern verificationPatter = Pattern.compile(".*(?<ver>[0-9]{8}).*");
         Matcher matcher = verificationPatter.matcher(comment);
@@ -37,20 +107,34 @@ public final class DocumentUtil {
         return "";
     }
 
-    private static ImportGrant grant(String prescriptionDate,
-                                     String deliveryDate,
-                                     String amount,
-                                     String invoiceNumber,
-                                     String verificationNumber) {
-        return new ImportGrant(
-                prescriptionDate, deliveryDate,
-                amount, invoiceNumber, verificationNumber);
-    }
-
+    /**
+     * Check if all strings in the array are empty.
+     * This method will not check if a string only contains whitespace.
+     *
+     * @param row should be a array with all fields initialized.
+     * @return true if all strings are empty
+     * @throws NullPointerException if any of the of the strings are null.
+     */
     public static boolean emptyRow(String[] row) {
         boolean empty = true;
         for (int i = 1; i < row.length; i++) {
             empty = empty & row[i].isEmpty();
+        }
+        return empty;
+    }
+
+    /**
+     * Check if all strings in the array are empty.
+     * This method will treat whitespace as an empty string.
+     *
+     * @param row should be a array with all fields initialized.
+     * @return true if all strings are empty
+     * @throws NullPointerException if any of the of the strings are null.
+     */
+    public static boolean emptyOrWhitespace(String[] row) {
+        boolean empty = true;
+        for (int i = 1; i < row.length; i++) {
+            empty = empty & row[i].trim().isEmpty();
         }
         return empty;
     }
--- core-bc/modules/application/src/main/java/se/vgregion/glasogonbidrag/util/IdentificationUtil.java
+++ core-bc/modules/application/src/main/java/se/vgregion/glasogonbidrag/util/IdentificationUtil.java
@@ -2,110 +2,141 @@ package se.vgregion.glasogonbidrag.util;
 
 import se.vgregion.glasogonbidrag.model.IdentificationType;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Martin Lind - Monator Technologies AB
  */
 public final class IdentificationUtil {
-    private IdentificationUtil() {
 
+    private static final Pattern personalNumberRegex;
+    private static final Pattern reserveNumberRegex;
+    static {
+        personalNumberRegex = Pattern.compile(
+                "(?<year>[0-9]{2})?" +
+                        "(?<number>[0-9]{6})\\-(?<control>[0-9]{4})");
+        reserveNumberRegex = Pattern.compile(
+                "(?<year>[0-9]{2})?" +
+                        "(?<number>[0-9]{6}\\-[A-Z0-9]{4})");
     }
 
-    public static IdentificationType detect(String id) {
+    /**
+     * Prevent initialization of this class.
+     */
+    private IdentificationUtil() {}
+
+    /**
+     * Detect the type the identification number is.
+     * The number can be a Valid, Invalid or Reserved number if it is in
+     * the correct format. It can also be None if it's neither a
+     * personal identification number or a reserve number.
+     *
+     * @param number the string to test if it's a identification number.
+     * @return the type the identification number is of.
+     */
+    public static IdentificationType detect(String number) {
         IdentificationType type = IdentificationType.NONE;
 
-        if (detectPersonalNumber(id)) {
-            if (validatePersonalNumber(id)) {
+        if (detectPersonalNumber(number)) {
+            if (validatePersonalNumber(number)) {
                 type = IdentificationType.VALID;
             } else {
                 type = IdentificationType.INVALID;
             }
-        } else if (detectReservedNumber(id)) {
+        } else if (detectReservedNumber(number)) {
             type = IdentificationType.RESERVE;
         }
 
         return type;
     }
 
+    /**
+     * Detect if the string is an personal identification number.
+     * It may or may not be a valid identification number but it should be
+     * in the correct format.
+     *
+     * @param number the number to test.
+     * @return true if the number follows the correct format.
+     */
     public static boolean detectPersonalNumber(String number) {
-        int length = number.length() - 1;
-
-        if (length != 10 && length != 12) {
-            return false;
-        }
-
-        String reversedNumber =
-                new StringBuilder(number).reverse().toString();
-
-        String[] split = number.split("-", 2);
-
-        return reversedNumber.charAt(4) == '-'
-                && split.length == 2
-                && !split[0].isEmpty() && !split[1].isEmpty()
-                && split[0].matches("[0-9]+")
-                && split[1].matches("[0-9]+")
-                && (split[0].length() == 6 || split[0].length() == 8)
-                && split[1].length() == 4;
+        Matcher matcher = personalNumberRegex.matcher(number);
+        return matcher.matches();
     }
 
+    /**
+     * Detect if the string is an reserve number.
+     * A personal identification number is also a correct reserve number.
+     *
+     * @param number the number to test.
+     * @return true if the number follows the correct format.
+     */
     public static boolean detectReservedNumber(String number) {
+        Matcher matcher = reserveNumberRegex.matcher(number);
+        return matcher.matches();
+    }
+
+    /**
+     * Validate a personal number. Checks if the control digit is
+     * correctly entered.
+     *
+     * @param number the number to validate.
+     * @return true if the identification number is correct.
+     */
+    public static boolean validatePersonalNumber(String number) {
+        String n = removeYear(number);
+        int[] ns = string2array(n);
+        return ibmMod10ver(ns);
+    }
+
+    // Transform identification numbers
+
+    /**
+     * Remove leading two digits if the number contains 12 digits.
+     *
+     * @param number the number to remove the year from.
+     * @return return a identification number with only the two least
+     *         significant digits from the year.
+     */
+    public static String removeYear(String number) {
         int length = number.length() - 1;
 
-        if (length != 10 && length != 12) {
-            return false;
-        }
-
-        String reversedNumber =
-                new StringBuilder(number).reverse().toString();
-        String[] split = number.split("-", 2);
-
-        return reversedNumber.charAt(4) == '-'
-                && split.length == 2
-                && !split[0].isEmpty() && !split[1].isEmpty()
-                && split[0].matches("[0-9]+")
-                && (split[0].length() == 6 || split[0].length() == 8)
-                && split[1].length() == 4;
-    }
-
-    public static boolean validatePersonalNumber(String id) {
-        String num = removeHyphen(removeYear(id));
-
-        int[] numbers = string2array(num);
-
-        return ibmMod10(numbers);
-    }
-
-    // Transform identificaiton numbers
-
-    public static String removeYear(String id) {
-        String numbers = removeHyphen(id);
-
-        if (numbers.length() == 12) {
-            return id.substring(2);
+        if (length == 12 || length == 10) {
+            return number.substring(2);
         } else {
-            return id;
+            return number;
         }
     }
 
-    public static String addYear(String id, int year) {
-        return null;
-    }
+    /**
+     * Removes the hyphen from an identitification number.
+     * Will return the original string if the string don't contains an
+     * hyphen.
+     *
+     * @param number to remove the hyphen from.
+     * @return a string without hyphen
+     */
+    public static String removeHyphen(String number) {
+        int index = number.indexOf('-');
 
-    public static String removeHyphen(String id) {
-        if (!id.contains("-")) {
-            return id;
+        if (index == -1) {
+            return number;
+        } else {
+            return number.substring(0, index)
+                    .concat(number.substring(index + 1));
         }
-
-        int index = id.indexOf('-');
-        return id.substring(0, index).concat(id.substring(index + 1));
-    }
-
-    public static String addHyphen(String id) {
-        return null;
     }
 
     // Helper methods
 
-    private static boolean ibmMod10(int[] numbers) {
+    /**
+     * Verification of the Luhn-algorithm (also known as IBM MOD10)
+     *
+     * @param numbers an array of 10 numbers which should be verified.
+     * @return true if the control digit (least significant) is
+     *         correct or not.
+     */
+    private static boolean ibmMod10ver(int[] numbers) {
         int[] pattern = new int[] {2,1,2,1,2,1,2,1,2,1};
 
         int sum = 0;
@@ -117,10 +148,18 @@ public final class IdentificationUtil {
         return sum % 10 == 0;
     }
 
-    private static int[] string2array(String id) {
+    /**
+     * Turn a string of numbers into a array of integers.
+     * This method only works when there the number are in the format
+     * YYMMDD-XXXX or YYMMDDXXXX, the hyphen will be removed by the code.
+     *
+     * @param number identification number to turn into a string.
+     * @return array of 10 integers.
+     */
+    private static int[] string2array(String number) {
         int[] is = new int[10];
 
-        char[] numbers = removeHyphen(id).toCharArray();
+        char[] numbers = removeHyphen(number).toCharArray();
         for (int i = 0; i < 10; i++) {
             is[i] = char2int(numbers[i]);
         }
