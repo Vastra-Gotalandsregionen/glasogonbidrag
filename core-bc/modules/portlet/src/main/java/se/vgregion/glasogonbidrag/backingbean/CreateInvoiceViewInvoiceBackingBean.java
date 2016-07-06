50325491f0352af3b9c235002822067c4d311d89
Erik Andersson <erik.andersson@monator.com>
2016-07-06 09:25:44 +0200

    Minor adjustments to add_grant and view_invoice. Also, added locale and portletNamespae to CreateInvoiceViewInvoiceBackingBean

--- core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/backingbean/CreateInvoiceViewInvoiceBackingBean.java
+++ core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/backingbean/CreateInvoiceViewInvoiceBackingBean.java
@@ -1,6 +1,7 @@
 package se.vgregion.glasogonbidrag.backingbean;
 
 
+import com.liferay.portal.theme.ThemeDisplay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
@@ -12,6 +13,8 @@ import se.vgregion.service.glasogonbidrag.api.data.InvoiceRepository;
 import se.vgregion.service.glasogonbidrag.api.service.InvoiceService;
 
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import java.util.Locale;
 
 /**
  * @author Martin Lind - Monator Technologies AB
@@ -32,7 +35,29 @@ public class CreateInvoiceViewInvoiceBackingBean {
     @Autowired
     private FacesUtil facesUtil;
 
+    // Helpers
+
+    private String portletNamespace;
+    private Locale locale;
+
+
     private Invoice invoice;
+
+    public String getPortletNamespace() {
+        return portletNamespace;
+    }
+
+    public void setPortletNamespace(String portletNamespace) {
+        this.portletNamespace = portletNamespace;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
 
     public Invoice getInvoice() {
         return invoice;
@@ -62,6 +87,15 @@ public class CreateInvoiceViewInvoiceBackingBean {
 
     @PostConstruct
     protected void init() {
+        ThemeDisplay themeDisplay = facesUtil.getThemeDisplay();
+        locale = themeDisplay.getLocale();
+        // Temporary - make sure we always get Swedish locale
+        locale = Locale.forLanguageTag("sv-SE");
+
+        portletNamespace = FacesContext.getCurrentInstance()
+                .getExternalContext().encodeNamespace("");
+
+
         Long invoiceId = facesUtil.fetchId("invoiceId");
 
         if (invoiceId != null) {
