71b8fd906b6e58a0a49221ba3e6b0f03b0be409a
Martin Lind <martin@spegelreflex.se>
2016-12-06 15:50:19 +0100

    Removed usage of low level database query service and use new filter methods.

--- core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/backingbean/ManageSuppliersViewBackingBean.java
+++ core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/backingbean/ManageSuppliersViewBackingBean.java
@@ -17,7 +17,6 @@ import se.vgregion.portal.glasogonbidrag.domain.dto.SupplierDTO;
 import se.vgregion.portal.glasogonbidrag.domain.jpa.Invoice;
 import se.vgregion.portal.glasogonbidrag.domain.jpa.Supplier;
 import se.vgregion.service.glasogonbidrag.domain.api.service.InvoiceService;
-import se.vgregion.service.glasogonbidrag.domain.api.service.LowLevelDatabaseQueryService;
 import se.vgregion.service.glasogonbidrag.domain.api.service.SupplierService;
 
 import javax.annotation.PostConstruct;
@@ -46,9 +45,6 @@ public class ManageSuppliersViewBackingBean {
 
     @Autowired
     private SupplierService supplierService;
-
-    @Autowired
-    private LowLevelDatabaseQueryService queryService;
 
     private SupplierLazyDataModel lazyDataModel;
     private SupplierInvoiceLazyDataModel invoiceLazyDataModel;
@@ -103,9 +99,7 @@ public class ManageSuppliersViewBackingBean {
     public void onRowSelect(SelectEvent event) {
         selectedSupplier = ((SupplierDTO) event.getObject()).getSupplier();
         invoiceLazyDataModel = new SupplierInvoiceLazyDataModel(
-                selectedSupplier.getId(), invoiceService, queryService);
-
-//        selectedSupplierInvoices = invoiceService.findAllBySupplier(selectedSupplier);
+                selectedSupplier.getId(), invoiceService);
     }
 
     public void onRowDeselect(UnselectEvent event) {
