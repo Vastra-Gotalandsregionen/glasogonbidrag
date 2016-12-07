ed7858b54d3a56707ebc48ebaf30767b3e238535
Martin Lind <martin@spegelreflex.se>
2016-12-07 09:59:37 +0100

    Updated backing bean to use new methods.

--- core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/datamodel/BeneficiaryLazyDataModel.java
+++ core-bc/modules/portlet/src/main/java/se/vgregion/glasogonbidrag/datamodel/BeneficiaryLazyDataModel.java
@@ -6,12 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.vgregion.service.glasogonbidrag.domain.api.service.BeneficiaryService;
 import se.vgregion.portal.glasogonbidrag.domain.dto.BeneficiaryDTO;
-import se.vgregion.service.glasogonbidrag.domain.api.service.LowLevelDatabaseQueryService;
-import se.vgregion.service.glasogonbidrag.types.LowLevelSortOrder;
 import se.vgregion.service.glasogonbidrag.types.OrderType;
+import se.vgregion.service.glasogonbidrag.types.lowlevel.BeneficiaryFilter;
+import se.vgregion.service.glasogonbidrag.types.lowlevel.BeneficiaryOrder;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,24 +21,10 @@ public class BeneficiaryLazyDataModel extends LazyDataModel<BeneficiaryDTO> {
     private final static Logger LOGGER =
             LoggerFactory.getLogger(BeneficiaryLazyDataModel.class);
 
-
-    private final static Map<String, String> VALUE_MAP = new HashMap<>();
-    static {
-        VALUE_MAP.put("number", "i.number");
-        VALUE_MAP.put("fullName", "b.fullName");
-        VALUE_MAP.put("count", "COUNT(g)");
-    }
-
-    private final static Map<String, String> FILTER_MAP = new HashMap<>();
-
     private final BeneficiaryService service;
-    private final LowLevelDatabaseQueryService lowLevelService;
 
-    public BeneficiaryLazyDataModel(
-            BeneficiaryService service,
-            LowLevelDatabaseQueryService lowLevelService) {
+    public BeneficiaryLazyDataModel(BeneficiaryService service) {
         this.service = service;
-        this.lowLevelService = lowLevelService;
     }
 
     @Override
@@ -59,22 +44,23 @@ public class BeneficiaryLazyDataModel extends LazyDataModel<BeneficiaryDTO> {
                                      String sortField,
                                      SortOrder sortOrder,
                                      Map<String, Object> filters) {
+        BeneficiaryFilter beneficiaryFilter = new BeneficiaryFilter(
+                filters);
+        BeneficiaryOrder beneficiaryOrder = new BeneficiaryOrder(
+                sortField,
+                OrderType.parse(sortOrder.toString()));
+
         List<BeneficiaryDTO> results;
-
-        LowLevelSortOrder sort = new LowLevelSortOrder(
-                sortField, "b.id",
-                OrderType.parse(sortOrder.toString()), filters,
-                VALUE_MAP, FILTER_MAP);
-
         try {
-            results = lowLevelService.listBeneficiaries(sort, first, pageSize);
+            results = service.findAllFiltered(
+                    first, pageSize, beneficiaryFilter, beneficiaryOrder);
         } catch (Exception e) {
             LOGGER.warn("Threw exception! {}", e.getMessage());
 
             results = new ArrayList<>();
         }
 
-        this.setRowCount(lowLevelService.countBeneficiaries(sort));
+        this.setRowCount(service.countFiltered(beneficiaryFilter));
 
         return results;
     }
