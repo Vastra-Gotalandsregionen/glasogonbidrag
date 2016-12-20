84a7bbf0697776e5faaea2716ff723f59a4b824d
Martin Lind <martin@spegelreflex.se>
2016-12-20 09:24:55 +0100

    Fixed errors in migration service. Must remove bindings before saving.

--- core-bc/composites/types/src/main/java/se/vgregion/portal/glasogonbidrag/domain/jpa/Migration.java
+++ core-bc/composites/types/src/main/java/se/vgregion/portal/glasogonbidrag/domain/jpa/Migration.java
@@ -1,12 +1,17 @@
 package se.vgregion.portal.glasogonbidrag.domain.jpa;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -21,30 +26,40 @@ import java.util.Set;
 @Table(name = "migration")
 @NamedQueries(value = {
         @NamedQuery(
-                name = "glasogonbidrag.migration.findAllWithPartsByDate",
-                query = "SELECT m " +
-                        "FROM Migration m " +
-                        "LEFT JOIN FETCH m.invoices " +
-                        "WHERE m.date = :date"),
-
-        @NamedQuery(
-                name = "glasogonbidrag.migration.findAllDates",
-                query = "SELECT DISTINCT m.date " +
+                name = "glasogonbidrag.migration.exists",
+                query = "SELECT CASE " +
+                        "   WHEN (COUNT(m) > 0) THEN TRUE " +
+                        "   ELSE FALSE " +
+                        "END " +
                         "FROM Migration m"),
+        @NamedQuery(
+                name = "glasogonbidrag.migration.findAll",
+                query = "SELECT m FROM Migration m ORDER BY m.date")
 })
 public class Migration {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "migration_date")
     private Date date;
 
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "migration")
-    private Set<Invoice> invoices;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+            name="beneficiary_id",
+            unique=true,
+            nullable=true)
+    private Beneficiary beneficiary;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+            name = "prescription_id",
+            unique=true,
+            nullable=true)
+    private Prescription prescription;
 
     public Migration() {
-        invoices = new HashSet<>();
     }
 
     public long getId() {
@@ -63,19 +78,19 @@ public class Migration {
         this.date = date;
     }
 
-    public Set<Invoice> getInvoices() {
-        return invoices;
+    public Beneficiary getBeneficiary() {
+        return beneficiary;
     }
 
-    public void setInvoices(Set<Invoice> invoices) {
-        this.invoices = invoices;
+    public void setBeneficiary(Beneficiary beneficiary) {
+        this.beneficiary = beneficiary;
     }
 
-    public void addInvoice(Invoice invoice) {
-        this.invoices.add(invoice);
+    public Prescription getPrescription() {
+        return prescription;
     }
 
-    public void removeInvoice(Invoice invoice) {
-        this.invoices.remove(invoice);
+    public void setPrescription(Prescription prescription) {
+        this.prescription = prescription;
     }
 }
